00d48008f618ada30a3a4af1a0c69fd54dc57cf5
JulianHernandez1944 <julhernandezar@unal.edu.co>
2025-05-31 09:05:44 -0500

    mlflow local 08

--- homework/src/main.py
+++ homework/src/main.py
@@ -40,8 +40,10 @@ def main():
     ##
     working_directory = os.path.abspath(os.getcwd())
     mlflow_runs_path = os.path.join(working_directory, "my_mlruns")
+
     if not os.path.exists(mlflow_runs_path):
         os.makedirs(mlflow_runs_path)
+
     mlflow.set_tracking_uri("file:" + mlflow_runs_path)
 
     ## Autotracking para sklearn
@@ -98,9 +100,4 @@ def main():
         mlflow.log_metric("test_mae", mae)
         mlflow.log_metric("test_r2", r2)
 
-        ## Ya no se requiere
-        # save_model_if_better(model, x_test, y_test)
-
-
-if __name__ == "__main__":
-    main()
+        
